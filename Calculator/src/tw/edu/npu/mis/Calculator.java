6d9f7e89838c9b989a7dde15368d6fd35b639d2d
Samael Wang <freesamael@gmail.com>
2015-06-18 02:41:56 +0800

    Defeature. Update.

--- Calculator/src/tw/edu/npu/mis/Calculator.java
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -41,6 +41,7 @@ public class Calculator extends Observable {
      * The internal state of the calculator.
      */
     public enum State {
+
         INPUT_OPERATED_VALUE,
         INPUT_OPERATOR,
         INPUT_OPERATING_VALUE,
@@ -51,13 +52,11 @@ public class Calculator extends Observable {
     private State mState;
     private String mOperatedValue;
     private String mOperatingValue;
-    private String mMemoryValue;
     private String mErrorMessage;
     private Operator mOperator;
 
     public Calculator() {
-        clear();
-        memoryClear();
+        performClear();
     }
 
     /**
@@ -78,9 +77,11 @@ public class Calculator extends Observable {
             } else {
                 mOperatedValue += String.valueOf(digit);
             }
+            setChanged();
         } else if (mState == State.INPUT_OPERATOR) {
             mState = State.INPUT_OPERATING_VALUE;
             mOperatingValue = String.valueOf(digit);
+            setChanged();
         } else if (mState == State.INPUT_OPERATING_VALUE
                 && mOperatingValue.length() < MAX_INPUT_LENGTH) {
             if ("0".equals(mOperatingValue)) {
@@ -88,13 +89,14 @@ public class Calculator extends Observable {
             } else {
                 mOperatingValue += String.valueOf(digit);
             }
+            setChanged();
         } else if (mState == State.OUTPUT_RESULT) {
             mState = State.INPUT_OPERATED_VALUE;
             mOperatedValue = String.valueOf(digit);
             mOperatingValue = "0";
+            setChanged();
         }
-        
-        setChanged();
+
         notifyObservers();
     }
 
@@ -106,18 +108,21 @@ public class Calculator extends Observable {
         if (mState == State.INPUT_OPERATED_VALUE
                 && !mOperatedValue.contains(".")) {
             mOperatedValue += ".";
+            setChanged();
         } else if (mState == State.INPUT_OPERATOR) {
             mState = State.INPUT_OPERATING_VALUE;
             mOperatingValue = "0.";
+            setChanged();
         } else if (mState == State.INPUT_OPERATING_VALUE
                 && !mOperatingValue.contains(".")) {
             mOperatingValue += ".";
+            setChanged();
         } else if (mState == State.OUTPUT_RESULT) {
             mState = State.INPUT_OPERATED_VALUE;
             mOperatedValue = "0.";
+            setChanged();
         }
-        
-        setChanged();
+
         notifyObservers();
     }
 
@@ -129,67 +134,160 @@ public class Calculator extends Observable {
     public void performOperation(Operator operator) {
         mErrorMessage = null;
         if (operator == Operator.EQUAL) {
-            if (mState == State.INPUT_OPERATOR) {
-                mOperatingValue = mOperatedValue;
-            }
-            mState = State.OUTPUT_RESULT;
-            calculateResult();
+            performEqual();
         } else if (operator == Operator.PLUS_MINUS) {
-            if (mState == State.INPUT_OPERATED_VALUE) {
-                mOperatedValue = String.valueOf(
-                        Double.parseDouble(mOperatedValue) * -1);
-            } else if (mState == State.INPUT_OPERATOR) {
-                mState = State.INPUT_OPERATING_VALUE;
-                mOperatingValue = String.valueOf(
-                        Double.parseDouble(mOperatedValue) * -1);
-            } else if (mState == State.INPUT_OPERATING_VALUE) {
-                mOperatingValue = String.valueOf(
-                        Double.parseDouble(mOperatingValue) * -1);
-            } else /* OUTPUT_RESULT */ {
-                mState = State.INPUT_OPERATED_VALUE;
-                mOperatedValue = String.valueOf(
-                        Double.parseDouble(mOperatedValue) * -1);
-            }
-        } else {
+            performPlusMinus();
+        } else if (operator == Operator.BACKSPACE) {
+            performBackspace();
+        } else if (operator == Operator.CLEAR_ENTRY) {
+            performClearEntry();
+        } else if (operator == Operator.CLEAR) {
+            performClear();
+        } else /* general operators */ {
             if (mState == State.INPUT_OPERATING_VALUE) {
                 calculateResult();
             }
             mState = State.INPUT_OPERATOR;
             mOperator = operator;
+            setChanged();
         }
-        
-        setChanged();
+
         notifyObservers();
     }
 
+    /**
+     * Calculate result.
+     */
+    private void performEqual() {
+        if (mState == State.INPUT_OPERATOR) {
+            mOperatingValue = mOperatedValue;
+        }
+        mState = State.OUTPUT_RESULT;
+        calculateResult();
+        setChanged();
+    }
+
+    /**
+     * Change the positive / negative sign.
+     */
+    private void performPlusMinus() {
+        if (mState == State.INPUT_OPERATED_VALUE
+                && !"0".equals(mOperatedValue)) {
+            mOperatedValue = formatDecimal(
+                    Double.parseDouble(mOperatedValue) * -1);
+        } else if (mState == State.INPUT_OPERATOR
+                && !"0".equals(mOperatedValue)) {
+            mState = State.INPUT_OPERATING_VALUE;
+            mOperatingValue = formatDecimal(
+                    Double.parseDouble(mOperatedValue) * -1);
+        } else if (mState == State.INPUT_OPERATING_VALUE
+                && !"0".equals(mOperatingValue)) {
+            mOperatingValue = formatDecimal(
+                    Double.parseDouble(mOperatingValue) * -1);
+        } else if (!"0".equals(mOperatedValue)) /* OUTPUT_RESULT */ {
+            mState = State.INPUT_OPERATED_VALUE;
+            mOperatedValue = formatDecimal(
+                    Double.parseDouble(mOperatedValue) * -1);
+        }
+        setChanged();
+    }
+
+    /**
+     * Delete the last digit in current input.
+     */
+    private void performBackspace() {
+        if (mState == State.OUTPUT_RESULT
+                || mState == State.INPUT_OPERATED_VALUE) {
+            mState = State.INPUT_OPERATED_VALUE;
+            if (mOperatedValue.length() > 1) {
+                mOperatedValue = mOperatedValue
+                        .substring(0, mOperatedValue.length() - 1);
+            } else {
+                mOperatedValue = "0";
+            }
+            setChanged();
+        } else if (mState == State.INPUT_OPERATOR
+                || mState == State.INPUT_OPERATING_VALUE) {
+            mState = State.INPUT_OPERATING_VALUE;
+            if (mOperatingValue.length() > 1) {
+                mOperatingValue = mOperatingValue
+                        .substring(0, mOperatingValue.length() - 1);
+            } else {
+                mOperatingValue = "0";
+            }
+            setChanged();
+        }
+    }
+
+    /**
+     * Clear current input.
+     */
+    private void performClearEntry() {
+        if (mState == State.OUTPUT_RESULT
+                || mState == State.INPUT_OPERATED_VALUE) {
+            mState = State.INPUT_OPERATED_VALUE;
+            mOperatedValue = "0";
+        } else if (mState == State.INPUT_OPERATOR
+                || mState == State.INPUT_OPERATING_VALUE) {
+            mState = State.INPUT_OPERATING_VALUE;
+            mOperatingValue = "0";
+        }
+        setChanged();
+    }
+
+    /**
+     * Clear the calculation.
+     */
+    private void performClear() {
+        mErrorMessage = null;
+        mState = State.OUTPUT_RESULT;
+        mOperator = null;
+        mOperatedValue = mOperatingValue = "0";
+        setChanged();
+    }
+
+    /**
+     * Make actual calculation.
+     */
     private void calculateResult() {
         double operatedValue = Double.parseDouble(mOperatedValue);
         double operatingValue = Double.parseDouble(mOperatingValue);
         Double result = null;
-        
-        switch (mOperator) {
-            case PLUS:
-                result = operatedValue + operatingValue;
-                break;
-            case MINUS:
-                result = operatedValue - operatingValue;
-                break;
-            case TIMES:
-                result = operatedValue * operatingValue;
-                break;
-            case OVER:
-                if (operatingValue == 0) {
-                    mErrorMessage = "Error - divided by zero.";
-                } else { 
-                    result = operatedValue / operatingValue;
-                }
-                break;
+
+        if (mOperator == Operator.PLUS) {
+            result = operatedValue + operatingValue;
+        } else if (mOperator == Operator.MINUS) {
+            result = operatedValue - operatingValue;
+        } else if (mOperator == Operator.TIMES) {
+            result = operatedValue * operatingValue;
+        } else if (mOperator == Operator.OVER) {
+            if (operatingValue == 0) {
+                mErrorMessage = "Error";
+                mOperatedValue = mOperatingValue = "0";
+            } else {
+                result = operatedValue / operatingValue;
+            }
         }
-        
+
         if (result != null) {
-            DecimalFormat fmt = new DecimalFormat("#.########");
-            mOperatedValue = fmt.format(result);
+            mOperatedValue = formatDecimal(result);
         }
+    }
+
+    /**
+     * Format decimal for better display.
+     *
+     * @param decimal Decimal number to format.
+     * @return Formated string.
+     */
+    private String formatDecimal(double decimal) {
+        DecimalFormat fmt = new DecimalFormat("#.########");
+        String value = fmt.format(decimal);
+        if (value.length() > 10) {
+            fmt = new DecimalFormat("0.######E0");
+            value = fmt.format(decimal);
+        }
+        return value;
     }
 
     /**
@@ -198,29 +296,13 @@ public class Calculator extends Observable {
      * @return A string to display.
      */
     public String getDisplay() {
-        if (mErrorMessage != null)
+        if (mErrorMessage != null) {
             return mErrorMessage;
-        
+        }
+
         if (mState == State.INPUT_OPERATING_VALUE) {
             return mOperatingValue;
         }
         return mOperatedValue;
-    }
-
-    /**
-     * Clear the calculation.
-     */
-    private void clear() {
-        mState = State.OUTPUT_RESULT;
-        mOperator = null;
-        mOperatedValue = "0";
-        mOperatingValue = "0";
-    }
-
-    /**
-     * Clear the memory value.
-     */
-    private void memoryClear() {
-        mMemoryValue = "0";
     }
 }
